011723a54d05808c96422f3f0ba63f93bee1e702
GoodTY <taek3153@gmail.com>
2020-05-20 18:03:30 +0900

    moveTo plan

--- Maze.java
+++ Maze.java
@@ -47,11 +47,21 @@ public class Maze {
     }
 
     private boolean moveTo(int row, int col) {
-
         /*
-              You code goes here ...
-         */
-        return false;
-    }
+        1st 주변이 0이 4,3,2,1개 일 경우
+            col 과 row가 큰 곳으로 이동
+        2nd 주변이 0이 없는 경우
+            i) 주변에 passed가 1개 있는 경우
+               passed로 이동
+            ii) 주변에 passed가 2개 이상 있는 경우
+                passed의 숫자가 낮은 곳으로 이동
+                만약 passed가 같다면 col과 row가 큰 곳으로 이동
 
+            iii) 주변에 passed가 없는 경우
+               return false
+        3rd exit에 도달한 경우
+            return true
+        return false;
+         */
+    }
 }
