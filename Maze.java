f9787a634e7e594cf32d7202f4018d8ff3e136ca
GoodTY <taek3153@gmail.com>
2020-05-21 23:21:34 +0900

    maze first complete
    but missing return ... why?

--- Maze.java
+++ Maze.java
@@ -47,187 +47,41 @@ public class Maze {
     }
 
     private boolean moveTo(int row, int col) {
-        Location position = entry;
-        int[] collect = null;
-        int count = 0;
-        int right = maze[position.row][position.col + 1];
-        int left = maze[position.row][position.col - 1];
-        int up = maze[position.row + 1][position.col];
-        int down = maze[position.row - 1][position.col];
-
-        if (position.row == 0) {
-            if (position.col == 0) { // 0,0
-                if (right == 1 || visited[position.row][position.col + 1] == true) { // 서쪽으로 이동 불가
-                    if (down == 1 || visited[position.row + 1][position.col] == true) { // 남쪽으로 이동 불가
-                        collect[count] = 0;
-                        count++;
-                        //움직일 곳이 없는 경우
-                    } else if (down == 0 && visited[position.row + 1][position.col] == false) {
-                        position.row += position.row;
-                        visited[position.row + 1][position.col] = true;
-                        moveTo(position.row, position.col);
-                    } else { // down == 0 && visited true
-                        collect[count] += 0;
-                        count++;
-                    }
-                } else if (right == 0 && visited[position.row][position.col + 1] == true) {
-                    if (down == 1 || visited[position.row + 1][position.col] == true) { // 남쪽으로 이동 불가
-                        collect[count] = 0;
-                        count++;
-                        //움직일 곳이 없는 경우
-                    } else if (down == 0 && visited[position.row + 1][position.col] == false) {
-                        position.row += position.row;
-                        visited[position.row + 1][position.col] = true;
-                        moveTo(position.row, position.col);
-                    } else { // down == 0 && visited true
-                        collect[count] += 0;
-                        count++;
-                    }
-                } else {
-                    moveTo(position.row, position.col + 1);
-                    if (down == 1 || visited[position.row + 1][position.col] == true) { // 남쪽으로 이동 불가
-                        collect[count] = 0;
-                        count++;
-                    } else if (down == 0 && visited[position.row + 1][position.col] == false) {
-                        visited[position.row + 1][position.col] = true;
-                        moveTo(position.row + 1, position.col);
-                    } else { // down == 0 && visited true
-                        collect[count] += 0;
-                        count++;
-                    }
-                }
-            } else if (position.col == numCols) { // (0,1) 좌 하 움직이기 가능
-                if (left == 1 || visited[position.row][position.col - 1] == true) { // 동쪽으로 이동 불가
-                    if (down == 1 || visited[position.row + 1][position.col] == true) { // 남쪽으로 이동 불가
-                        collect[count] = 0;
-                        count++;
-                        //움직일 곳이 없는 경우
-                    } else if (down == 0 && visited[position.row + 1][position.col] == false) {
-                        position.row += position.row;
-                        visited[position.row + 1][position.col] = true;
-                        moveTo(position.row, position.col);
-                    } else { // down == 0 && visited true
-                        collect[count] += 0;
-                        count++;
-                    }
-                } else if (left == 0 && visited[position.row][position.col - 1] == true) { // 동쪽 이동 불가
-                    if (down == 1 || visited[position.row + 1][position.col] == true) { // 남쪽으로 이동 불가
-                        collect[count] = 0;
-                        count++;
-                        //움직일 곳이 없는 경우
-                    } else if (down == 0 && visited[position.row + 1][position.col] == false) {
-                        position.row += position.row;
-                        visited[position.row + 1][position.col] = true;
-                        moveTo(position.row, position.col);
-                    } else { // down == 0 && visited true
-                        collect[count] += 0;
-                        count++;
-                    }
-                } else { //동쪽 이동 가능
-                    moveTo(position.row, position.col + 1);
-                    if (down == 1 || visited[position.row + 1][position.col] == true) { // 남쪽으로 이동 불가
-                        collect[count] = 0;
-                        count++;
-                    } else if (down == 0 && visited[position.row + 1][position.col] == false) {
-                        visited[position.row + 1][position.col] = true;
-                        moveTo(position.row + 1, position.col);
-                    } else { // down == 0 && visited true
-                        collect[count] += 0;
-                        count++;
-                    }
-                }
-
-            } else { // 좌 우 하 이동 가능
-                if (left == 1 || visited[position.row][position.col - 1] == true) { // 동쪽으로 이동 불가
-                    if (down == 1 || visited[position.row + 1][position.col] == true) { // 남쪽으로 이동 불가
-                        collect[count] = 0;
-                        count++;
-                        //움직일 곳이 없는 경우
-                    } else if (down == 0 && visited[position.row + 1][position.col] == false) {
-                        position.row += position.row;
-                        visited[position.row + 1][position.col] = true;
-                        moveTo(position.row, position.col);
-                    } else { // down == 0 && visited true
-                        collect[count] += 0;
-                        count++;
-                    }
-                } else if (left == 0 && visited[position.row][position.col - 1] == true) { // 동쪽 이동 불가
-                    if (down == 1 || visited[position.row + 1][position.col] == true) { // 남쪽으로 이동 불가
-                        collect[count] = 0;
-                        count++;
-                        //움직일 곳이 없는 경우
-                    } else if (down == 0 && visited[position.row + 1][position.col] == false) {
-                        position.row += position.row;
-                        visited[position.row + 1][position.col] = true;
-                        moveTo(position.row, position.col);
-                    } else { // down == 0 && visited true
-                        collect[count] += 0;
-                        count++;
-                    }
-                } else { //동쪽 이동 가능
-                    moveTo(position.row, position.col + 1);
-                    if (down == 1 || visited[position.row + 1][position.col] == true) { // 남쪽으로 이동 불가
-                        collect[count] = 0;
-                        count++;
-                    } else if (down == 0 && visited[position.row + 1][position.col] == false) {
-                        visited[position.row + 1][position.col] = true;
-                        moveTo(position.row + 1, position.col);
-                    } else { // down == 0 && visited true
-                        collect[count] += 0;
-                        count++;
-                    }
-                }
-            }
-            visited[position.row][position.col] = true;
+        if (row == exit.row && col == exit.col) {
+            return true;
         }
-
-
-
-
-
-        ///////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-
-
-
-        else if(position.row == numRows){
-            if (position.col == 0) {
-
-            }
-            else if (position.col == numCols) {
-
-            }
-            else {
-            }
-        }
-
         else {
-            if (position.col == 0) {
-
-            }
-            else if (position.col == numCols){
-
-            }
-            else {
-
+            if (row < 0 || row >= numRows || col < 0 || col >= numCols) {
+                int right = maze[row][col + 1], left = maze[row][col - 1],
+                        up = maze[row + 1][col], down = maze[row - 1][col];
+                boolean visitR = visited[row][col + 1], visitL = visited[row][col - 1],
+                        visitU = visited[row - 1][col], visitD = visited[row + 1][col];
+                if (right == 0 && visitR == false && col + 1 < numCols) {
+                    moveTo(row, col + 1);
+                    visited[row][col] = true;
+                } else if (left == 0 && visitL == false && col - 1 >= 0) {
+                    moveTo(row, col - 1);
+                    visited[row][col] = true;
+                } else if (up == 0 && visitU == false && row - 1 >= 0) {
+                    moveTo(row - 1, col);
+                    visited[row][col] = true;
+                } else if (down == 0 && visitD == false && row + 1 < numCols) {
+                    moveTo(row + 1, col);
+                    visited[row][col] = true;
+                } else {
+                    if (row == entry.row && col == entry.col) {
+                        if ((right == 1 || (right == 0 && visitR == true) || col + 1 >= numCols) && (left == 1 || (left == 0 && visitL == true) || col - 1 < 0) &&
+                                (up == 1 || (up == 0 && visitU == true) || row - 1 < 0) &&
+                                (down == 1 || (down == 0 && visitD == true) || row + 1 >= numRows)) {
+                            return false;
+                        }
+                    }
+                    else{
+                        moveTo(row + 1, col);
+                        visited[row][col] = true;
+                    }
+                }
             }
         }
-        /*
-        1st 주변이 0이 4,3,2,1개 일 경우
-            모든 moveTo(0 좌표) 생성
-        2nd 주변이 0이 없는 경우
-            i) 주변에 passed가 1개 있는 경우
-               passed로 이동
-            ii) 주변에 passed가 2개 이상 있는 경우
-                passed의 숫자가 낮은 곳으로 이동
-                만약 passed가 같다면 col과 row가 큰 곳으로 이동
-
-            iii) 주변에 passed가 없는 경우
-               return false
-        3rd exit에 도달한 경우
-            return true
-        return false;
-         */
     }
 }
